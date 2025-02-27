d4164bfea93b85d71b7346b641b6a24ff2592f70
Ateeq-Raheman <ateeqraheman001195@gmail.com>
2025-02-27 12:03:12 +0530

    changed the hooks file

--- custom_pos/hooks.py
+++ custom_pos/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/custom_pos/css/custom_pos.css"
-app_include_js = "/assets/custom_pos/js/custom_pos.js"
+app_include_js = "/assets/custom_pos/js/custom.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/custom_pos/css/custom_pos.css"
